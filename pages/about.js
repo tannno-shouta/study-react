69c116555200dad4dc6222d4d03d557edf48a225
tannno-shouta <71753010+tannno-shouta@users.noreply.github.com>
2023-01-25 11:10:31 +0900

    ヘッドライン部分をコンポーネント化

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Links } from '../components/Links'
+import Headline from '../components/Headline'
 
 export default function Home() {
   return (
@@ -13,54 +13,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className={styles.main}>
-      <h1 className = {styles.title}>About Page</h1>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/about.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-          <Links />
-          <Footer />
+        <Headline title="About Page" page="about"/>
+        <Links />
+        <Footer />
       </main>
     </>
   )
